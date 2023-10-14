fc8c411d3cf5f545afc4159554250aa44baf582b
letyresina <leticiaresina@gmail.com>
2023-10-14 02:01:01 -0300

    feito a home (sem responsividade)

--- script/script.js
+++ script/script.js
@@ -4,3 +4,12 @@ const secaoAlvo = document.getElementById('conteudoHome');
 paraScrollar.addEventListener('click', function() {
     secaoAlvo.scrollIntoView({ behavior: 'smooth', duration:1.8});
 });
+
+
+function abrirTechGirls(){
+    window.open("techgirls.html")
+}
+
+function abrirProjeto(){
+    window.open("projeto.html")
+}
